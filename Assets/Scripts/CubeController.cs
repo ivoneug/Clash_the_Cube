1a6d95748001df95c75687962c0ef283abf212d2
Evgeniy Ivon <ivoneug@gmail.com>
2021-01-31 15:06:17 +0300

    added pushing towards force removed bouncing fixed cube show animation added rim light

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -112,7 +112,7 @@ public class CubeController : MonoBehaviour
 
         SetNumber(number);
 
-        transform.DOScale(0f, 0f).Play();
+        transform.localScale = Vector3.zero;
         transform.DOScale(1f, 0.5f).SetEase(Ease.OutQuad).Play();
     }
 
--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -10,6 +10,10 @@ namespace ClashTheCube
 
         [SerializeField] private IntVariable nextCubeNumber;
         [SerializeField] private Vector3Variable nextCubePosition;
+        [SerializeField] private FloatReference cubeMergeSideForceMultiplier;
+        [SerializeField] private FloatReference cubeMergeUpForceMultiplier;
+        [SerializeField] private FloatReference cubeMergeMinDistanceToApplyForce;
+        [SerializeField] private FloatReference cubeMergeMaxBackDistanceToApplyForce;
         [SerializeField] private FloatReference cubeMergeForceMin;
         [SerializeField] private FloatReference cubeMergeForceMax;
         [SerializeField] private FloatReference cubeMergeAngle;
@@ -36,12 +40,74 @@ namespace ClashTheCube
 
         public void SpawnMerge()
         {
-            var quaternion = Quaternion.Euler(Random.Range(-cubeMergeAngle, cubeMergeAngle), 0f, Random.Range(-cubeMergeAngle, cubeMergeAngle));
+            // var quaternion = Quaternion.Euler(Random.Range(-cubeMergeAngle, cubeMergeAngle), 0f, Random.Range(-cubeMergeAngle, cubeMergeAngle));
+            var quaternion = Quaternion.identity;
+            // var force = Vector3.up;
+            var force = Vector3.zero;
+            var torque = (new Vector3(Random.Range(-cubeMergeAngle, cubeMergeAngle), 0f, Random.Range(-cubeMergeAngle, cubeMergeAngle))).normalized;
+
+            var nearestCube = GetNearestMatchingCube();
+            if (nearestCube != null)
+            {
+                var direction = (nearestCube.transform.position - nextCubePosition.Value).normalized * cubeMergeSideForceMultiplier;
+                force = direction + Vector3.up * cubeMergeUpForceMultiplier;
+            }
+            else
+            {
+                force = Vector3.up * cubeMergeUpForceMultiplier;
+            }
 
             var cube = Instantiate(cubePrefab, nextCubePosition.Value, quaternion).GetComponent<CubeController>();
             cube.InitMerged(nextCubeNumber.Value);
-            cube.Body.AddForce(Vector3.up * Random.Range(cubeMergeForceMin, cubeMergeForceMax));
-            cube.Body.AddTorque(Vector.Vector3RandomNormal() * cubeMergeTorque, ForceMode.Impulse);
+
+            cube.Body.AddForce(force * Random.Range(cubeMergeForceMin, cubeMergeForceMax));
+            // cube.Body.AddTorque(Vector.Vector3RandomNormal() * cubeMergeTorque, ForceMode.Impulse);
+            cube.Body.AddTorque(torque * cubeMergeTorque, ForceMode.Impulse);
+        }
+
+        private CubeController GetNearestMatchingCube()
+        {
+            var objects = GameObject.FindGameObjectsWithTag("Cube");
+            CubeController nearest = null;
+
+            foreach (var obj in objects)
+            {
+                var cube = obj.GetComponent<CubeController>();
+                if (cube.Number != nextCubeNumber.Value ||
+                    cube.State != CubeController.CubeState.Transition)
+                {
+                    continue;
+                }
+
+                if (nearest == null)
+                {
+                    nearest = cube;
+                }
+                else
+                {
+                    var distance1 = Vector.DistanceTo(nextCubePosition.Value, nearest.transform.position);
+                    var distance2 = Vector.DistanceTo(nextCubePosition.Value, cube.transform.position);
+
+                    nearest = distance2 < distance1 ? cube : nearest;
+                }
+            }
+
+            if (nearest != null)
+            {
+                var nearestPos = nearest.transform.position;
+
+                if (Vector.DistanceTo(nextCubePosition.Value, nearestPos) < cubeMergeMinDistanceToApplyForce)
+                {
+                    nearest = null;
+                }
+                else if (nextCubePosition.Value.z > nearestPos.z &&
+                         Vector.DistanceTo(nextCubePosition.Value, nearestPos) > cubeMergeMaxBackDistanceToApplyForce)
+                {
+                    nearest = null;
+                }
+            }
+
+            return nearest;
         }
     }
 }
