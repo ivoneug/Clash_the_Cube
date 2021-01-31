84188d8a4d2f2a69774f6dc7f15b9fdae45706ff
Evgeniy Ivon <ivoneug@gmail.com>
2021-01-31 17:20:37 +0300

    switched target arch to android added fps and app version to the screen

--- Assets/Scripts/Framework/Utils/FPSCounter.cs
+++ Assets/Scripts/Framework/Utils/FPSCounter.cs
@@ -6,12 +6,24 @@ namespace Framework.Utils
 {
     public class FPSCounter : MonoBehaviour
     {
+        public enum FpsStyle
+        {
+            Fps,
+            Msec,
+            FpsMsec
+        }
+
+        [SerializeField] private FpsStyle fpsStyle = FpsStyle.FpsMsec;
+
         float deltaTime = 0.0f;
         GUIStyle style;
         Rect rect;
 
-        [SerializeField]
-        TextAnchor anchor = TextAnchor.LowerLeft;
+        [SerializeField] Font font;
+        [SerializeField][Range(1, 8)] int fontSize = 4;
+        [SerializeField] Color color = Color.white;
+        [SerializeField] TextAnchor anchor = TextAnchor.LowerRight;
+        [SerializeField] int inset = 0;
 
         void Update()
         {
@@ -25,18 +37,42 @@ namespace Framework.Utils
 
         void OnGUI()
         {
-            int w = Screen.width, h = Screen.height;
             if (style == null)
             {
+                int w = Screen.width - inset * 2;
+                int h = Screen.height - inset * 2;
+
                 style = new GUIStyle();
-                rect = new Rect(0, 0, w, h);
+                rect = new Rect(inset, inset, w, h);
                 style.alignment = anchor;
-                style.fontSize = h * 4 / 100;
-                style.normal.textColor = Color.white;
+                style.fontSize = h * fontSize / 100;
+                style.normal.textColor = color;
+
+                if (font)
+                {
+                    style.font = font;
+                }
             }
+
             float msec = deltaTime * 1000.0f;
             float fps = 1.0f / deltaTime;
-            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+
+            string text = string.Empty;
+            switch (fpsStyle)
+            {
+                case FpsStyle.Fps:
+                    text = string.Format("{0:0.} fps", fps);
+                    break;
+
+                case FpsStyle.Msec:
+                    text = string.Format("{0:0.0} ms", msec);
+                    break;
+
+                case FpsStyle.FpsMsec:
+                    text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+                    break;
+            }
+
             GUI.Label(rect, text, style);
         }
     }
