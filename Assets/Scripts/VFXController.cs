f420965009dd44e67b22f8301b9453d14484c195
Evgeniy Ivon <ivoneug@gmail.com>
2021-02-04 22:15:32 +0300

    added vfx controller and help arrows

---
+++ Assets/Scripts/VFXController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClashTheCube
+{
+    public class VFXController : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem[] helpArrows;
+        [SerializeField][Range(0, 45)] private int showHelpArrowsDelay = 15;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            
+        }
+        
+        public void ShowArrows()
+        {
+            SetArrowsActive(true);
+            CancelInvoke("ShowArrows");
+        }
+
+        public void HideArrows()
+        {
+            SetArrowsActive(false);
+            CancelInvoke("ShowArrows");
+            Invoke("ShowArrows", showHelpArrowsDelay);
+        }
+
+        private void SetArrowsActive(bool active)
+        {
+            foreach (var arrow in helpArrows)
+            {
+                if (active && !arrow.isPlaying)
+                {
+                    arrow.Play();
+                }
+                else if (!active && arrow.isPlaying)
+                {
+                    arrow.Stop();
+                }
+            }
+        }
+    }
+}
