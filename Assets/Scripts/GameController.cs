a47290da01d80be6d3919de456d55f5d540ef521
Evgeniy Ivon <ivoneug@gmail.com>
2021-02-06 00:03:33 +0300

    added settings button & menu implemented game paused state

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Framework.Variables;
 
 namespace ClashTheCube
 {
     public class GameController : MonoBehaviour
     {
+        [SerializeField] private BoolReference gamePaused;
+
         private void Start()
         {
+            ResumeGame();
+
             try
             {
                 Vibration.Init();
@@ -22,6 +27,23 @@ namespace ClashTheCube
                 Vibration.VibratePop();
             }
             catch { }
+        }
+
+        public void PauseGame()
+        {
+            gamePaused.Variable.SetValue(true);
+            UpdateGamePause();
+        }
+
+        public void ResumeGame()
+        {
+            gamePaused.Variable.SetValue(false);
+            UpdateGamePause();
+        }
+
+        private void UpdateGamePause()
+        {
+            Time.timeScale = gamePaused ? 0 : 1;
         }
     }
 }
