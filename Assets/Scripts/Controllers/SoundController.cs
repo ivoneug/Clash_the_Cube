b870efd10545d90c35380f07f6c0e520c1dd5efe
Evgeniy Ivon <ivoneug@gmail.com>
2021-02-06 23:09:11 +0300

    added sound controller and sounds

---
+++ Assets/Scripts/Controllers/SoundController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine.Soundy;
+
+namespace ClashTheCube
+{
+    public class SoundController : MonoBehaviour
+    {
+        [Header("Cube Merge Sound")]
+        [SerializeField] private SoundyData cubeMergeSound;
+
+        [Header("Achievement Sound")]
+        [SerializeField] private SoundyData achievementSound;
+
+        [Header("Game Over Sound")]
+        [SerializeField] private SoundyData gameOverSound;
+
+        public void PlayCubeMergeSound()
+        {
+            SoundyManager.Play(cubeMergeSound);
+        }
+
+        public void PlayAchievementSound()
+        {
+            SoundyManager.Play(achievementSound);
+        }
+
+        public void PlayGameOverSound()
+        {
+            SoundyManager.Play(gameOverSound);
+        }
+    }
+}
