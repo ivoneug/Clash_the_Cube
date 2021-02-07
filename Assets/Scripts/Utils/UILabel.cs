968d2e23beef338ec9ea9bbfe2413def5f9e1933
Evgeniy Ivon <ivoneug@gmail.com>
2021-02-07 21:18:18 +0300

    added uilabel class for localisation of complex textmesh pro label

---
+++ Assets/Scripts/Utils/UILabel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UILabel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI label;
+    [SerializeField] private TextMeshProUGUI shadow;
+    private TextMeshProUGUI parent;
+
+    public string text {
+        get { return label.text; }
+        set
+        {
+            label.text = value;
+            if (shadow)
+            {
+                shadow.text = value;
+            }
+            if (parent)
+            {
+                parent.text = value;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        parent = GetComponent<TextMeshProUGUI>();
+    }
+}
